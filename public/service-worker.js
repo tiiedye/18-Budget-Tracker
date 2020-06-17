6cfaa535b4214db6a26332e7558c95fc05576d6c
Julia Clements <tiiedye@gmail.com>
2020-06-17 10:35:36 -0600

    adding to files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,8 @@ const FILES_TO_CACHE = [
     "icons/icon-192x192.png",
     "icons/icon-512x512.png",
     "manifest.webmanifest",
-    "index.js"
+    "index.js",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
   
   const CACHE_NAME = "static-cache-v2";
